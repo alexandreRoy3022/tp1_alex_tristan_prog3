b4588dc14e63216ad969ad6cf44db40382e4e895
6224222 <6224222@cstjean.qc.ca>
2024-09-24 14:53:29 -0400

    CRÉATION MENU

---
+++ library/app.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+paned = ttk.PanedWindow(root, orient=tk.HORIZONTAL)
+paned.pack(fill=tk.BOTH, expand=1)
+
+frame1 = ttk.Frame(paned, width=400, height=300, relief=tk.RAISED)
+frame2 = ttk.Frame(paned, width=400, height=300, relief=tk.RAISED)
+
+paned.add(frame1)
+paned.add(frame2)
+
+label = tk.Label(frame2, text="Bienvenu au menu principal", width=100)
+label.pack()
+
+root.mainloop()
