681f02e118179245ed68943adcdec30e13938424
6224222 <6224222@cstjean.qc.ca>
2024-10-04 11:22:42 -0400

    CRÉATION MENU

--- library/app.py
+++ library/app.py
@@ -1,18 +1,32 @@
 
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Label, Button, PanedWindow, Frame
 
-root = tk.Tk()
-paned = ttk.PanedWindow(root, orient=tk.HORIZONTAL)
-paned.pack(fill=tk.BOTH, expand=1)
+from action.db import Database
 
-frame1 = ttk.Frame(paned, width=400, height=300, relief=tk.RAISED)
-frame2 = ttk.Frame(paned, width=400, height=300, relief=tk.RAISED)
+class Application:
+    root = tk.Tk()
+    paned = PanedWindow(root, orient=tk.HORIZONTAL)
+    paned.pack(fill=tk.BOTH, expand=1)
 
-paned.add(frame1)
-paned.add(frame2)
+    frame1 = Frame(paned, width=400, height=300, relief=tk.RAISED)
+    frame2 = Frame(paned, width=400, height=300, relief=tk.RAISED)
 
-label = tk.Label(frame2, text="Bienvenu au menu principal", width=100)
-label.pack()
+    paned.add(frame1)
+    paned.add(frame2)
+
+    label = Label(frame2, text="Bienvenu au menu principal", font=("Helvetica", 60))
+    label.pack()
+
+
+    bouton_ajouter_partie = Button(frame1, text="Ajouter partie", command=Database.ajouter_partie)
+    bouton_ajouter_partie.pack()
+
+
+    bouton_modifier_partie = Button(frame1, text="Modifier Partie", command=Database.modifier_partie)
+    bouton_modifier_partie.pack()
+
+    bouton_supprimer_partie = Button(frame1, text="Supprimer Partie", command=Database.supprimer_partie)
+    bouton_supprimer_partie.pack()
 
 root.mainloop()
