05f68606a27665b5deafcee575545acb35ca0382
Alexandre <6224222@cstjean.qc.ca>
2024-10-05 12:59:02 -0400

    avancement menu

---
+++ action/partie.py
@@ -0,0 +1,10 @@
+class Partie:
+    def __init__(self, nom_joueur1, nom_joueur2, date, niveau_joueur_1, niveau_joueur_2, type_partie, duree, resultat):
+        self.nom_joueur1 = nom_joueur1
+        self.nom_joueur2 = nom_joueur2
+        self.date = date
+        self.niveau_joueur_1 = niveau_joueur_1
+        self.niveau_joueur_2 = niveau_joueur_2
+        self.type_partie = type_partie
+        self.duree = duree
+        self.resultat = resultat
--- library/app.py
+++ library/app.py
@@ -1,32 +1,30 @@
 
-import tkinter as tk
-from tkinter import Label, Button, PanedWindow, Frame
+from tkinter import *
 
 from action.db import Database
+from action.partie import Partie
+
 
 class Application:
-    root = tk.Tk()
-    paned = PanedWindow(root, orient=tk.HORIZONTAL)
-    paned.pack(fill=tk.BOTH, expand=1)
+    def __init__(self):
+        self.root = Tk()
+        self.panedwindow = PanedWindow()
+        self.frame1 = Frame(self.panedwindow, width=400, height=300, relief=RAISED)
+        self.frame2 = Frame(self.panedwindow, width=400, height=300, relief=RAISED)
+        self.panedwindow.add(self.frame1)
+        self.panedwindow.add(self.frame2)
+        self.titre = Label(self.frame2, text="Bienvenu au menu principal", font=("Helvetica", 60))
+        self.bouton_ajouter_partie = Button(self.frame1, text="Ajouter partie", command=Database.ajouter_partie)
+        self.bouton_modifier_partie = Button(self.frame1, text="Modifier Partie", command=Database.modifier_partie)
+        self.bouton_supprimer_partie = Button(self.frame1, text="Supprimer Partie", command=Database.supprimer_partie)
+        self.panedwindow.pack(fill=BOTH, expand=True)
+        self.frame1.pack()
+        self.frame2.pack()
+        self.titre.pack()
+        self.bouton_ajouter_partie.pack()
+        self.bouton_modifier_partie.pack()
+        self.bouton_supprimer_partie.pack()
 
-    frame1 = Frame(paned, width=400, height=300, relief=tk.RAISED)
-    frame2 = Frame(paned, width=400, height=300, relief=tk.RAISED)
 
-    paned.add(frame1)
-    paned.add(frame2)
-
-    label = Label(frame2, text="Bienvenu au menu principal", font=("Helvetica", 60))
-    label.pack()
-
-
-    bouton_ajouter_partie = Button(frame1, text="Ajouter partie", command=Database.ajouter_partie)
-    bouton_ajouter_partie.pack()
-
-
-    bouton_modifier_partie = Button(frame1, text="Modifier Partie", command=Database.modifier_partie)
-    bouton_modifier_partie.pack()
-
-    bouton_supprimer_partie = Button(frame1, text="Supprimer Partie", command=Database.supprimer_partie)
-    bouton_supprimer_partie.pack()
 
 root.mainloop()
